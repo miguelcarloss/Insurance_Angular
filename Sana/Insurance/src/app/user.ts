d362507d90058aaae1e91f9b27c63e8d9b6df2cd
Miguel Santos <santos.mcs17@gmail.com>
2018-06-13 16:57:00 -0400

    latest push

---
+++ Sana/Insurance/src/app/user.ts
@@ -0,0 +1,19 @@
+export class User{
+    id: number;
+    fname: String;
+    lname: String;
+    uname: String;
+    email: String;
+    zipcode: number;
+    spouse: boolean;
+    children: number; //index
+    age: number;   //index
+    gender: number; //index
+    smoker: boolean;
+    hbp: boolean;
+    diabetes: boolean;
+    surgery: boolean;
+    allergies: boolean;
+    plan_mult: number;
+    plan_bonus: number;
+}

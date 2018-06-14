ada8dbb131c7aa957d9477e63ca096f16bfc5feb
Miguel Santos <santos.mcs17@gmail.com>
2018-06-13 20:42:11 -0400

    added my components

---
+++ Sana/Insurance/src/app/components/family/family.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-family',
+  templateUrl: './family.component.html',
+  styleUrls: ['./family.component.css']
+})
+export class FamilyComponent implements OnInit {
+  childrenOptions: Array<Object> = [
+    { id: 0, text: "zero" },
+    { id: 1, text: "one" },
+    { id: 2, text: "two" },
+    { id: 3, text: "three" },
+    { id: 4, text: "four or more" }
+  ];
+  spouseOptions: Array<Object> = [
+    { id: 0, text: "zero" },
+    { id: 1, text: "one" }
+  ];
+
+  selectedChild = this.childrenOptions[0];
+  selectedSpouse = this.spouseOptions[0];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  doStuff(){
+    console.log("primary: " + 1 );
+    console.log("spouse index: " + this.selectedSpouse.id);
+    console.log("child index: " + this.selectedChild.id);
+  }
+
+}
